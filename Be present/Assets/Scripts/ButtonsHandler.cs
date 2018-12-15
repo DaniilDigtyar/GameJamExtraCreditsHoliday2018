e8782338b035e3298e4c0b0ec26901506e21cc3d
Daniil Digtyar <daniildigtyar@gmail.com>
2018-12-15 12:33:02 +0100

    Menu added.

---
+++ Be present/Assets/Scripts/ButtonsHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsHandler : MonoBehaviour
+{
+    public void ButtonCredits()
+    {
+        Invoke("Credits", 0.5f);
+    }
+    private void Credits()
+    {
+        Application.LoadLevel("Credits");
+    }
+
+    public void ButtonMenu()
+    {
+        Invoke("Menu", 0.5f);
+    }
+    private void Menu()
+    {
+        Application.LoadLevel("MenuPrincipal");
+    }
+
+    public void ButtonPlay()
+    {
+        Invoke("Play", 0.5f);
+    }
+    private void Play()
+    {
+        Application.LoadLevel("Main");
+    }
+
+    public void ButtonInstructions()
+    {
+        Invoke("Instructions", 0.5f);
+    }
+    private void Instructions()
+    {
+        Application.LoadLevel("Instructions");
+    }
+
+    public void ButtonExit()
+    {
+        Invoke("Exit", 0.5f);
+    }
+    private void Exit()
+    {
+        Application.Quit();
+    }
+}
